5770206e76cc2a449af9e87b71f36c642c36daec
resoul <resoul@bk.ru>
2025-11-01 02:27:04 +0200

    demo project

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { AppRouting } from '@/routing/app-routing';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AuthProvider } from './auth/providers/supabase-provider';
+import { SettingsProvider } from './providers/settings-provider';
+import { ThemeProvider } from './providers/theme-provider';
+import { I18nProvider } from './providers/i18n-provider';
+import { BrowserRouter } from 'react-router-dom';
+import { LoadingBarContainer } from 'react-top-loading-bar';
+import { HelmetProvider } from 'react-helmet-async';
+import { TooltipsProvider } from './providers/tooltips-provider';
+import { QueryProvider } from './providers/query-provider';
+import { Toaster } from '@/components/ui/sonner';
+import { ModulesProvider } from './providers/modules-provider';
 
-function App() {
-  const [count, setCount] = useState(0)
+const { BASE_URL } = import.meta.env;
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export function App() {
+    const queryClient = new QueryClient();
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <AuthProvider>
+                <SettingsProvider>
+                    <ThemeProvider>
+                        <I18nProvider>
+                            <HelmetProvider>
+                                <TooltipsProvider>
+                                    <QueryProvider>
+                                        <LoadingBarContainer>
+                                            <BrowserRouter basename={BASE_URL}>
+                                                <Toaster />
+                                                <ModulesProvider>
+                                                    <AppRouting />
+                                                </ModulesProvider>
+                                            </BrowserRouter>
+                                        </LoadingBarContainer>
+                                    </QueryProvider>
+                                </TooltipsProvider>
+                            </HelmetProvider>
+                        </I18nProvider>
+                    </ThemeProvider>
+                </SettingsProvider>
+            </AuthProvider>
+        </QueryClientProvider>
+    )
 }
-
-export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+import { fileURLToPath, URL } from 'node:url';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react(), tailwindcss()],
+    base: '/',
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+    },
+    build: {
+        chunkSizeWarningLimit: 3000,
+    },
 })
